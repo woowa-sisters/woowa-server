45c3745a77a255285a9cca1453b1bdafc356d0b1
yena <yeena114@naver.com>
2024-01-26 21:09:26 +0900

    fix: meeting 테스트 코드 변경

--- src/test/java/com/woowaSisters/woowaSisters/WoowaSistersApplicationTests.java
+++ src/test/java/com/woowaSisters/woowaSisters/WoowaSistersApplicationTests.java
@@ -1,15 +1,6 @@
 package com.woowaSisters.woowaSisters;
-/*
-import com.woowaSisters.woowaSisters.domain.bookmark.Bookmark;
-import com.woowaSisters.woowaSisters.domain.bookmark.BookmarkRepository;*//*
-import com.woowaSisters.woowaSisters.domain.community.Community;
-import com.woowaSisters.woowaSisters.domain.community.CommunityRepository;*/
+
 import com.woowaSisters.woowaSisters.domain.meeting.Meeting;
-import com.woowaSisters.woowaSisters.domain.park.ParkRepository;
-import com.woowaSisters.woowaSisters.domain.park.Parks;
-import com.woowaSisters.woowaSisters.domain.test.TestRepository;
-import com.woowaSisters.woowaSisters.domain.user.User;
-import com.woowaSisters.woowaSisters.domain.user.UserRepository;
 import com.woowaSisters.woowaSisters.service.MeetingService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -28,7 +19,6 @@ import static junit.framework.TestCase.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@SpringBootApplication
 public class WoowaSistersApplicationTests {
 
 	@Autowired
@@ -38,8 +28,9 @@ public class WoowaSistersApplicationTests {
 	public void createMeetingTest() {
 		// 테스트용 Meeting 객체 생성
 		Meeting meeting = new Meeting();
-		meeting.setMeetingTitle("Test Meeting");
-		meeting.setMeetingTime(Instant.parse("2024-02-01T10:00:00").toEpochMilli());
+		meeting.setMeetingTitle("Test Meeting2");
+		meeting.setMeetingAttendees(3);
+		meeting.setMeetingLocation("home");
 
 		// 모임 생성 메서드 호출
 		Meeting createdMeeting = meetingService.createMeeting(meeting);
@@ -51,6 +42,6 @@ public class WoowaSistersApplicationTests {
 		assertNotNull(createdMeeting.getId());
 
 		// 생성된 모임의 제목이 올바르게 설정되었는지 확인
-		assertEquals("Test Meeting", createdMeeting.getMeetingTitle());
+		assertEquals("Test Meeting2", createdMeeting.getMeetingTitle());
 	}
 }
