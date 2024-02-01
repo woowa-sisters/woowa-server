177ae359b2ad8a1eb178670d9a9136777011db56
yena <yeena114@naver.com>
2024-02-02 01:30:42 +0900

    REFACTOR: 모임 구독 클래스 제거

--- src/main/java/com/woowaSisters/woowaSisters/WoowaSistersApplication.java
+++ src/main/java/com/woowaSisters/woowaSisters/WoowaSistersApplication.java
@@ -7,8 +7,8 @@ import com.woowaSisters.woowaSisters.domain.community.CommunityRepository;
 import com.woowaSisters.woowaSisters.domain.meeting.Meeting;
 import com.woowaSisters.woowaSisters.domain.meeting.MeetingRepository;
 import com.woowaSisters.woowaSisters.domain.park.ParkRepository;
-import com.woowaSisters.woowaSisters.domain.subscribeMeeting.SubscribeMeeting;
-import com.woowaSisters.woowaSisters.domain.subscribeMeeting.SubscribeMeetingRepository;
+/*import com.woowaSisters.woowaSisters.domain.subscribeMeeting.SubscribeMeeting;
+import com.woowaSisters.woowaSisters.domain.subscribeMeeting.SubscribeMeetingRepository;*/
 import com.woowaSisters.woowaSisters.domain.test.Test;
 import com.woowaSisters.woowaSisters.domain.test.TestRepository;
 import com.woowaSisters.woowaSisters.domain.user.UserRepository;
@@ -20,8 +20,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = {Parks.class, User.class, Meeting.class, Bookmark.class, Community.class, Test.class, SubscribeMeeting.class})
-@EnableJpaRepositories (basePackageClasses = {UserRepository.class, ParkRepository.class, MeetingRepository.class, BookmarkRepository.class, CommunityRepository.class, TestRepository.class, SubscribeMeetingRepository.class})
+@EntityScan(basePackageClasses = {Parks.class, User.class, Meeting.class, Bookmark.class, Community.class, Test.class})
+@EnableJpaRepositories (basePackageClasses = {UserRepository.class, ParkRepository.class, MeetingRepository.class, BookmarkRepository.class, CommunityRepository.class, TestRepository.class})
 
 public class WoowaSistersApplication {
 
